9cd79a3426614163bd7a25606ba1c640265cbfb3
Felipe <felipe.oliveira.ger@gmail.com>
2024-08-11 20:45:04 -0300

    Alteração na função VerificaBipartido

--- VerificaBipartido.cpp
+++ VerificaBipartido.cpp
@@ -12,7 +12,11 @@ int GrafoDir::VerificaBipartido(){
     int vertices_coloridos{0};
     std::queue<int> fila;
     fila.push(0);
-    coloracao[0] = AZUL;
+    if(listaAdj[0].size() != 0)
+        coloracao[0] = AZUL;
+    else 
+        coloracao[0] = VERMELHO;
+    vertices_coloridos++;
     while(vertices_coloridos < qtdVertices){
         while(!fila.empty()){
             int atual = fila.front();
@@ -20,10 +24,14 @@ int GrafoDir::VerificaBipartido(){
             for(int i = 0; i < listaAdj[atual].size(); i++){
                 Aresta a = listaAdj[atual][i];
                 if(coloracao[a.v_entrada] == BRANCO){
-                    if(coloracao[atual] == AZUL)
+                    if(coloracao[atual] == AZUL){
                         coloracao[a.v_entrada] = VERMELHO;
-                    else
+                        vertices_coloridos++;
+                    }
+                    else{
                         coloracao[a.v_entrada] = AZUL;
+                        vertices_coloridos++;
+                    }
                     fila.push(a.v_entrada);
                 }
                 else if(coloracao[a.v_entrada] == coloracao[atual])
@@ -34,9 +42,13 @@ int GrafoDir::VerificaBipartido(){
         bool achou = false;
         while(i < qtdVertices && achou == false){
             if(coloracao[i] == BRANCO){
-                coloracao[i] = AZUL;
+                if(listaAdj[i].size() != 0)
+                    coloracao[i] = AZUL;
+                else 
+                    coloracao[i] = VERMELHO;
                 fila.push(i);
                 achou = true;
+                vertices_coloridos++;
             }
         i++;
         }
